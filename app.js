9a21b678941f36babe44ac8d6a9a60b63fc37a5b
stammer1 <stammer786@gmail.com>
2024-12-15 16:51:30 -0800

    Disk Storage added

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const app = express();
+const multer = require('multer');
+const path = require('path');
+const crypto = require('crypto');
 
 app.set("view engine" , "ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+const goodam = multer.diskStorage ({
+    destination: function(req, file , cb){
+  cb(null, './Public/images/uploads')
+    },
+    filename: function(req, file , cb){
+        crypto.randomBytes(12 ,function(err ,bytes){
+            const fn = bytes.toString("hex") + path.extname(file.originalname);
+            cb(null, fn)
+        })
+    }
+})
 
-app.post('/upload' , function(req, res){
-    
+const upload = multer({storage : goodam})
+app.post('/upload' , upload.single("images") , function(req, res){
+    res.send("File is Uploaded");
 })
 
 
-app.get('/' , function(req ,res){
+app.get('/' ,function(req ,res){
     res.render("text");
 })
 
